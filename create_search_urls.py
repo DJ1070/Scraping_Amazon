143950ea52a6c9007445abe30e46f01973384708
Diana Jaffé <74904333+DJ1070@users.noreply.github.com>
2021-02-25 09:19:35 +0100

    Add files via upload

--- create_search_urls.py
+++ create_search_urls.py
@@ -4,7 +4,7 @@ f.write(url)
 f.write('\n')
 
 
-for i in range(2,51):
+for i in range(2,401):
     url = 'https://www.amazon.com/s?k=Processor&i=computers-intl-ship&page=' + str(i) + '&qid=1594555641&ref=sr_pg_' + str(i)
     f.write(url)
     f.write('\n')
