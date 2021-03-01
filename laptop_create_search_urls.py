2527dc96e91dbd94a8ae7719fd9bf279d7314a33
Diana Jaffé <74904333+DJ1070@users.noreply.github.com>
2021-03-01 15:13:24 +0100

    Add files via upload

--- laptop_create_search_urls.py
+++ laptop_create_search_urls.py
@@ -1,11 +1,10 @@
 f = open('laptop_search_urls.txt', 'w+')
-url = 'https://www.amazon.com/s?k=laptop&i=computers&__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss_1'
+url = 'https://www.amazon.com/s?k=laptop&i=computers&language=en_US&ref=nb_sb_noss_1'
 f.write(url)
 f.write('\n')
 
-
 for i in range(2,401):
-    url = 'https://www.amazon.com/-/de/s?k=laptop&i=computers&page=' + str(i) + '&__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&qid=1613478173&ref=sr_pg_' + str(i)
+    url = 'https://www.amazon.com/s?k=laptop&i=computers&page=' + str(i) + '&language=en_US&qid=1614601629&ref=sr_pg_' + str(i)
     f.write(url)
     f.write('\n')
 
